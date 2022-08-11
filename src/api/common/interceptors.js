273e72ec7404a891961cc2223f27f26eb11f4b9e
jangjaehyeon <jja33705@gmail.com>
2022-08-11 12:27:43 +0900

    jwt token cookie에 저장으로 변경

--- src/api/common/interceptors.js
+++ src/api/common/interceptors.js
@@ -1,38 +1,32 @@
-import userStore from "../../store/modules/user";
-
 function setInterceptors(instance) {
-  instance.interceptors.request.use(
-    function (config) {
-      // 요청 보내기 전에 헤더에 토큰 넣어줌
-      config.headers.Authorization = `Bearer ${userStore.state.accessToken}`;
-      return config;
-    },
-    function (error) {
-      return Promise.reject(error);
-    }
-  );
-
-  instance.interceptors.response.use(
-    function (response) {
-      return response;
-    },
-    async (error) => {
-      if (error.response.status === 401) {
-        if (error.response.data.message === "TokenExpired") {
-          const {
-            data: { accessToken },
-          } = await instance.post("/users/refreshToken");
-
-          // vuex에 access token 갱신
-          userStore.state.accessToken = accessToken;
-          return instance(error.config);
-        }
+    instance.interceptors.request.use(
+      function (config) {
+        return config;
+      },
+      function (error) {
+        return Promise.reject(error);
       }
-      return Promise.reject(error);
-    }
-  );
-
-  return instance;
-}
-
-export default setInterceptors;
+    );
+  
+    instance.interceptors.response.use(
+      function (response) {
+        return response;
+      },
+      async (error) => {
+        if (error.response.status === 401) {
+          if (error.response.data.message === "TokenExpired") {
+              const { status } = await instance.post("/users/refreshToken");
+              if (status === 200) {
+                  return instance(error.config);
+              }
+          }
+        }
+        return Promise.reject(error);
+      }
+    );
+  
+    return instance;
+  }
+  
+  export default setInterceptors;
+  
